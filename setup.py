36fdd886535742a091718f2b96f18ad6c43cd0ba
Mario D Amore <mariodamore@pbophelia.local>
2015-12-14 18:00:40 +0100

    first setup.py version

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='multibinner',
+      version='0.1',
+      description='dataset multibinner based on pandas',
+      author="Mario D'Amore",
+      author_email='kidpixo☄@gmail.com',
+      url='https://github.com/kidpixo/multibinner/',
+      packages=['multibinner'],
+      install_requires=['numpy','pandas']
+     )
